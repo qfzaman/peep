8295fcda953fab6a657c22304b1e24a438a3734c
Quazi Fahmiduzzaman <quazi.fahmiduzzaman@gmail.com>
2024-01-12 21:35:23 +0600

    Refactor Auth with afterAuth req/auth

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,19 @@
-import { authMiddleware, redirectToSignIn } from "@clerk/nextjs";
+import { authMiddleware } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
  
 export default authMiddleware({
 
 publicRoutes: ["/"],
 
-  afterAuth() {
+  afterAuth(auth, req) {
+    // Handle users who aren't authenticated
+    if (!auth.userId && !auth.isPublicRoute) {
+      return NextResponse.redirect(new URL("/", req.url));
+    }
+    // If the user is logged in and trying to access a protected route, allow them to access route
+    if (auth.userId && !auth.isPublicRoute) {
+      return NextResponse.next();
+    }
     // Allow users visiting public routes to access them
     return NextResponse.next();
   },
