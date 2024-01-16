33feb4faf9940384cc0abb3206f96bbe8c101673
Quazi Fahmiduzzaman <quazi.fahmiduzzaman@gmail.com>
2024-01-16 23:18:15 +0600

    Render posts on profile page

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -6,6 +6,31 @@ import { createTRPCRouter, privateProcedure, publicProcedure } from "~/server/ap
 import { Ratelimit } from "@upstash/ratelimit"; // for deno: see above
 import { Redis } from "@upstash/redis"; // see below for cloudflare and fastly adapters
 import { filterUserForClient } from "~/server/helpers/filterUserForClient";
+import type { Post } from "@prisma/client";
+
+const addUserDataToPosts = async (posts: Post[]) => {
+      const users = (await clerkClient.users.getUserList({
+      userId: posts.map((post) => post.authorId),
+      limit: 100,
+    })).map(filterUserForClient);
+
+  return posts.map((post) => {
+    const author = users.find((user) => user.id === post.authorId);
+
+    if (!author?.username) throw new TRPCError({
+      code: "INTERNAL_SERVER_ERROR",
+      message: "Author for post not found",
+    });
+
+    return {
+      post,
+      author: {
+        ...author,
+        username: author.username,
+      }
+    };
+  });
+}
 
 // Create a new ratelimiter, that allows 2 requests per 1 minute
 const ratelimit = new Ratelimit({
@@ -23,29 +48,20 @@ export const postRouter = createTRPCRouter({
         {createdAt: "desc"}
       ],
     });
-
-    const users = (await clerkClient.users.getUserList({
-      userId: posts.map((post) => post.authorId),
-      limit: 100,
-    })).map(filterUserForClient);
-
-    return posts.map((post) => {
-      const author = users.find((user) => user.id === post.authorId);
-
-      if (!author?.username) throw new TRPCError({
-        code: "INTERNAL_SERVER_ERROR",
-        message: "Author for post not found",
-      });
-
-      return {
-        post,
-        author: {
-          ...author,
-          username: author.username,
-        }
-      };
-    });
+    return addUserDataToPosts(posts);
   }),
+
+  getPostsByUserId: publicProcedure.input(z.object({
+    userId: z.string(),
+  })).query(({ ctx, input }) => ctx.db.post.findMany({
+    where: {
+      authorId: input.userId,
+    },
+    take: 100,
+    orderBy: [
+        {createdAt: "desc"}
+      ],
+  }).then(addUserDataToPosts)),
 
   create: privateProcedure.input(z.object({
     content: z.string().emoji("Only emojis are allowed").min(1).max(280),
