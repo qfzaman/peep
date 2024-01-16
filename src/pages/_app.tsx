6a99a711e4805f76dc53135fd047143a331136a0
Quazi Fahmiduzzaman <quazi.fahmiduzzaman@gmail.com>
2024-01-16 13:32:13 +0600

    CSS changes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -19,7 +19,7 @@ const MyApp: AppType = ({ Component, pageProps }) => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Toaster position="bottom-center" />
-      <Component {...pageProps} />;
+      <Component {...pageProps} />
     </ClerkProvider>
   );
 };
